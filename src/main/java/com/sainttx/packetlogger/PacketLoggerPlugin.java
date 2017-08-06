9291463960411c9ceb6d4a0d47da11ba71b883d5
Matthew <matthew.steglinski@utoronto.ca>
2017-08-06 14:04:06 -0400

    Initial commit

---
+++ src/main/java/com/sainttx/packetlogger/PacketLoggerPlugin.java
@@ -0,0 +1,123 @@
+package com.sainttx.packetlogger;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.comphenix.protocol.ProtocolLibrary;
+
+public class PacketLoggerPlugin extends JavaPlugin {
+    
+    public static String INTERCEPTING_PACKETS_META = "packetlogger-intercepting";
+    
+    @Override
+    public void onEnable() {
+        ProtocolLibrary.getProtocolManager().addPacketListener(new ReceivingPacketInterceptor(this));
+    }
+    
+    @Override
+    public void onDisable() {
+        getServer().getOnlinePlayers().forEach(p -> p.removeMetadata(INTERCEPTING_PACKETS_META, this));
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 0 || args.length < 2) {
+            return false;
+        }
+        
+        Player target = Bukkit.getPlayer(args[1]);
+        if (target == null) {
+            sender.sendMessage("\"" + args[1] + "\" is not online");
+            return true;
+        }
+        
+        if (args[0].equalsIgnoreCase("log")) {
+            if (isBeingTracked(target)) {
+                sender.sendMessage(target.getName() + " is already being logged");
+            } else {
+                target.setMetadata(INTERCEPTING_PACKETS_META, new FixedMetadataValue(this, new ArrayList<String>()));
+                sender.sendMessage("Now logging the packets of " + target.getName());
+            }
+            return true;
+        } else if (args[0].equalsIgnoreCase("dump")) {
+            if (!isBeingTracked(target)) {
+                sender.sendMessage(target.getName() + " is not being logged");
+            } else {
+                List<String> packets = getPacketLog(target);
+                File output = new File(getDataFolder(), "dump-" + target.getName() + ".txt");
+                try {
+                    if (!getDataFolder().exists()) {
+                        getDataFolder().mkdirs();
+                    }
+                    if (!output.exists()) {
+                        output.createNewFile();
+                    }
+
+                    try (FileWriter writer = new FileWriter(output, false)) {
+                        for (String packet : packets) {
+                            writer.write(packet);
+                        }
+                    }
+                    
+                    sender.sendMessage("Successfully dumped the packet log of " + target.getName() + " to " + output.getAbsolutePath());
+                } catch (IOException e) {
+                    sender.sendMessage("An error occured when dumping the packet log of " + target.getName());
+                    getLogger().log(Level.SEVERE, "dumping packets", e);
+                    return true;
+                }
+                
+                target.removeMetadata(INTERCEPTING_PACKETS_META, this);
+                sender.sendMessage("No longer tracking the packets of " + target.getName());
+            }
+            return true;
+        } else if (args[0].equalsIgnoreCase("clear")) {
+            if (!isBeingTracked(target)) {
+                sender.sendMessage(target.getName() + " is not being logged");
+            } else {
+                target.removeMetadata(INTERCEPTING_PACKETS_META, this);
+                sender.sendMessage("No longer tracking the packets of " + target.getName());
+            }
+            return true;
+        }
+            
+        return false;
+    }
+
+    /**
+     * Returns whether a player is having their packets intercepted.
+     * 
+     * @param player The player to intercept
+     * @return True if the plugin should inter
+     */
+    public boolean isBeingTracked(Player player) {
+        return player.hasMetadata(INTERCEPTING_PACKETS_META)
+                && player.getMetadata(INTERCEPTING_PACKETS_META).get(0).value() instanceof List;
+    }
+
+    /**
+     * Returns a current players packet log
+     * 
+     * @param player Player to fetch log from
+     * @return Players log
+     * @throws IllegalArgumentException if player is not being tracked
+     */
+    @SuppressWarnings("unchecked")
+    public List<String> getPacketLog(Player player) {
+       if (!isBeingTracked(player)) {
+           throw new IllegalArgumentException("player not tracked");
+       }
+       return (List<String>) player.getMetadata(INTERCEPTING_PACKETS_META).get(0).value();
+    }
+    
+}
---
+++ src/main/java/com/sainttx/packetlogger/ReceivingPacketInterceptor.java
@@ -0,0 +1,80 @@
+package com.sainttx.packetlogger;
+
+import static com.comphenix.protocol.PacketType.Play.Client.*;
+
+import java.lang.reflect.Field;
+import java.util.logging.Level;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
+
+/**
+ * Intercepts all received packets from a player
+ */
+public class ReceivingPacketInterceptor extends PacketAdapter {
+
+    public ReceivingPacketInterceptor(Plugin plugin) {
+        super(plugin, TELEPORT_ACCEPT, AUTO_RECIPE, TAB_COMPLETE, CHAT, 
+                CLIENT_COMMAND, SETTINGS, TRANSACTION, ENCHANT_ITEM, 
+                WINDOW_CLICK, CLOSE_WINDOW, CUSTOM_PAYLOAD, USE_ENTITY, 
+                KEEP_ALIVE, FLYING, POSITION, POSITION_LOOK, LOOK, VEHICLE_MOVE,
+                BOAT_MOVE, ABILITIES, BLOCK_DIG, ENTITY_ACTION, STEER_VEHICLE, 
+                RECIPE_DISPLAYED, RESOURCE_PACK_STATUS, ADVANCEMENTS, 
+                HELD_ITEM_SLOT, SET_CREATIVE_SLOT, UPDATE_SIGN, ARM_ANIMATION, 
+                SPECTATE, USE_ITEM, BLOCK_PLACE);
+    }
+    
+    @Override
+    public PacketLoggerPlugin getPlugin() {
+        return (PacketLoggerPlugin) super.getPlugin();
+    }
+    
+    @Override
+    public void onPacketReceiving(PacketEvent event) {
+        Player player = event.getPlayer();
+        
+        if (!getPlugin().isBeingTracked(player)) {
+            return;
+        }
+        
+        Object packet = event.getPacket().getHandle();
+        Class<?> packetClass = packet.getClass();
+        StringBuilder packetInformation = new StringBuilder();
+        
+        try {
+            dumpPacket(packetClass, packet, packetInformation, "");
+        } catch (Exception e) {
+            getPlugin().getLogger().log(Level.SEVERE, "failed to save packet information", e);
+            return;
+        }
+        
+        String output = packetInformation.toString();
+        getPlugin().getPacketLog(player).add(output);
+    }
+    
+    // Recursively prints packet fields and any superclass fields
+    private void dumpPacket(Class<?> packetClass, Object packet, StringBuilder packetInformation, String indent) {
+        Field[] fields = packetClass.getDeclaredFields();
+        if (fields.length > 0) {
+            packetInformation.append(indent).append(packetClass.getSimpleName()).append('\n');
+            try {
+                for (Field declaredField : fields) {
+                    declaredField.setAccessible(true);
+                    Object value = declaredField.get(packet);
+                    packetInformation.append(indent).append("  ")
+                            .append(declaredField.getName()).append('=').append(value)
+                            .append('\n');
+                }
+            } catch (Throwable t) {
+                throw new RuntimeException(t);
+            }
+        }
+        
+        if (packetClass.getSuperclass() != null) {
+            dumpPacket(packetClass.getSuperclass(), packet, packetInformation, indent + "  ");
+        }
+    }
+}
